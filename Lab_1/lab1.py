c8bdc02463e06e9952ab23dc21b16edf6a4d3621
Stevewalsh1989 <stevewalsh89@gmail.com>
2020-02-06 22:39:29 +0000

    working :)

--- Lab_1/lab1.py
+++ Lab_1/lab1.py
@@ -59,19 +59,16 @@ def solve(my_data):
         y += 1
         x = 0  # reset y value for each row
         for val in row:
-           # print(f"row : {row}")
-           #  print(f"val : {val}")
-
             if val == 'o':  # check point is not a mine
 
                 # print(f"Value : {val}")
-                #print(f" (x, y )  : ({x}, {y})")
+                # print(f" (x, y )  : ({x}, {y})")
                 search_points = get_search_points(x, y, my_data['NumRows']-1, my_data['NumColumns']-1)
 
-                num_mines = count_mines(x, y, search_points, my_data )
+                num_mines = count_mines(x, y, search_points, my_data)
                 val = str(num_mines)
 
-                # my_data["Matrix"][x][y] = val
+                my_data["Matrix"][y][x] = val
             x += 1  # increment y
     # print(f"Solved Result: {my_data}")
 
@@ -80,22 +77,17 @@ def solve(my_data):
 
 def count_mines(x, y, search_points, data):
 
-    # Points_pos Ord:Tp left - 0  Tp C - 1,    Tp right- 2,   left- 3,     right- 4,  btm left 5,    btm C- 6,  btm right- 7
+    # Points_pos :  Top Left  Top Center   Top Right   ->    [0]  [1]  [2]
+    #                   Left                 Right     ->    [3]       [4]
+    #               Btm Left  Btm Center   Btm Right   ->    [5]  [6]  [7]
     points_pos = [[x - 1, y - 1], [x, y - 1], [x + 1, y - 1], [x - 1, y], [x + 1, y], [x - 1, y + 1], [x, y + 1],
                   [x + 1, y + 1]]
     count = 0
-    vx = x
-    vy = y
-    # print(f"Search x : {vx}")
-    # print(f"Search y : {vy}")
     for point in search_points:
-        print(f" Point Pos {point} : {points_pos[point]}    ")
-
-    #     print(f"Search Point : {points_pos[point]}")
-    #
-        print(f"Search Point X : {points_pos[point][0]}")
-        print(f"Search Point y : {points_pos[point][1]}")
-        print(f"Value Found: {data['Matrix'][points_pos[point][1]][points_pos[point][0]]}")
+        # print(f" Point Pos {point} : {points_pos[point]} ")
+        # print(f"Search Point X : {points_pos[point][0]}")
+        # print(f"Search Point y : {points_pos[point][1]}")
+        # print(f"Value Found: {data['Matrix'][points_pos[point][1]][points_pos[point][0]]}")
         if data["Matrix"][points_pos[point][1]][points_pos[point][0]] == 'x':
             count += 1
             # print(f"count: {count}")
@@ -105,52 +97,32 @@ def count_mines(x, y, search_points, data):
 
 
 def get_search_points(x, y, last_row, last_cols):
-    print(f"--------------------------------")
+    # print(f"--------------------------------")
     # print(f" last_row : {last_row}")
     # print(f" last_cols : {last_cols}")
-
-    print(f" Search Points:  (x, y )  : ({x}, {y})")
-
-    # Scenarios: 9: Corners, Sides, else
+    # print(f" Search Points:  (x, y )  : ({x}, {y})")
 
     # Rules: If Corner
-    if x == 0 and y == 0:                  # 1 top left
-        print("top Left")
+    if x == 0 and y == 0:                   # 1 top left
         points = [4, 6, 7]
     elif x == 0 and y == last_cols:         # 2 btm left
-        print("btm Left")
-
         points = [1, 2, 4]
-    elif x == last_row and y == 0:         # 3 top right
-        print("top right")
-
+    elif x == last_row and y == 0:          # 3 top right
         points = [3, 5, 6]
     elif x == last_row and y == last_cols:  # 4 btm right
         points = [0, 1, 3]
-        print("btm right")
-
     # Rule: Sides
-    elif x == 0 and y != 0 and y != last_cols:          # 5 left
+    elif x == 0 and y != 0 and y != last_cols:         # 5 left
         points = [1, 2, 4, 6, 7]
-        print("left")
-
     elif y != 0 and y != last_cols and x == last_row:  # 6 right
         points = [0, 1, 3, 5, 6]
-        print("right")
-
-    elif x != 0 and y == 0 and x != last_row:         # 7 top
+    elif x != 0 and y == 0 and x != last_row:          # 7 top
         points = [3, 4, 5, 6, 7]
-        print("top")
-
-    elif x != 0 and x != last_row and y == last_cols:    # 8 bottom
+    elif x != 0 and x != last_row and y == last_cols:  # 8 bottom
         points = [0, 1, 2, 3, 4]
-        print("bottom")
-
     # Rule: 9 Else - search all position
     else:
-        print("mid")
-
-        points = [0, 1, 2, 3, 4, 5, 6, 7]  # all positions
+        points = [0, 1, 2, 3, 4, 5, 6, 7]  # Middle
 
     return points
 
