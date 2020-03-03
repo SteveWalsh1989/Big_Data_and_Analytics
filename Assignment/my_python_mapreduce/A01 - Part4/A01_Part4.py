a007a8255942d9aa607643dab30c6e47f9653f51
Stevewalsh1989 <stevewalsh89@gmail.com>
2020-03-03 20:26:35 +0000

    part 4 - parts 4/5 done i think - not sure what to do for 6

--- Assignment/my_python_mapreduce/A01 - Part4/A01_Part4.py
+++ Assignment/my_python_mapreduce/A01 - Part4/A01_Part4.py
@@ -15,7 +15,7 @@
 import os
 import codecs
 
-
+stations = []
 # ------------------------------------------
 # FUNCTION my_reduce
 # ------------------------------------------
@@ -45,6 +45,37 @@ def find_empty_station(my_list):
     for line in my_list:
         if line[1] == 0:                    # check if there are bikes available
             res.append([line[0], line[1]])  # add to result
+
+    # 3. We return res
+    return res
+
+
+# ------------------------------------------
+# FUNCTION check_station
+# ------------------------------------------
+def check_station(line):
+    # 1. We create the output variable
+    res = False
+
+    # 2. We check if name is in stations list
+    for name in stations:
+        if line[0] == name:
+            res = True
+
+    #3. We return res
+    return res
+
+
+# ------------------------------------------
+# FUNCTION add_bike
+# ------------------------------------------
+def add_bike(line):
+    # 1. We create the output variable
+    res = []
+
+    # 2. We make each entry to be (Station_name, 1)
+    res.append(line[0])
+    res.append(1)
 
     # 3. We return res
     return res
@@ -141,7 +172,8 @@ def process_line(line):
 # FUNCTION my_main
 # ------------------------------------------
 def my_main(my_list, station_names):
-
+    global stations
+    stations= station_names
     # --------------- STEP 1 ------------------------#
 
     # 1. Apply the Higher-Order function my_map provided above,
@@ -168,7 +200,7 @@ def my_main(my_list, station_names):
     # 3. Apply the Higher-Order function my_filter provided above,
     #    now to restrict only the entries which are ran out of bikes
 
-    my_list = find_empty_station(my_list) # TODO  -> Replace None with a call to my_filter
+    my_list = find_empty_station(my_list)
 
     print("\n\n\n\n\n------ STEP 3 ------\n")
     for item in range(50):
@@ -179,7 +211,7 @@ def my_main(my_list, station_names):
     # 4. Apply the Higher-Order function my_filter again,
     #    now to restrict the entries to the ones of the desired stations
 
-    my_list = None # TODO  -> Replace None with a call to my_filter
+    my_list = my_filter(check_station, my_list)
 
     print("\n\n\n\n\n------ STEP 4 ------\n")
     for item in range(50):
@@ -190,7 +222,7 @@ def my_main(my_list, station_names):
     # 5. Apply the the Higher-Order function my_map again,
     #    now to make each entry to be (Station_name, 1)
 
-    my_list = None  # TODO  -> Replace None with a call to my_map
+    my_list = my_map(add_bike, my_list)
 
     print("\n\n\n\n\n------ STEP 5 ------\n")
     for item in range(50):
